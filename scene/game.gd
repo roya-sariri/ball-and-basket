e04992936cd7d83e5cdb681baad2c8c447a821c0
unknown <saririroya@gmail.com>
2023-11-24 00:18:04 +0330

    Add pause

--- scene/game.gd
+++ scene/game.gd
@@ -9,7 +9,9 @@ var instance_basket_scene = basket_scene.instantiate()
 
 func _ready():
 	$Panel3.hide()
-	_create_time()
+	$Popup.hide()
+	$exit_texture_button.pressed.connect(exit)
+	create_time()
 	add_child(instance_ball_scene)
 	add_child(instance_basket_scene)
 
@@ -25,25 +27,25 @@ func _on_button_pressed(basket, ball):
 		var level = $Panel2.get_node("level_number_label").text.to_int() + 1
 		$Panel2.get_node("level_number_label").set_text(str(level))
 
-func _choose_ball_and_basket_random_image():
+func choose_ball_and_basket_random_image():
 	var script2d_ball = instance_ball_scene.get_node("Sprite2D")
 	script2d_ball.randomize_image()
 	instance_basket_scene.choose_random_image_for_each_basket()
 
-func _create_time():
+func create_time():
 	countdown_timer = Timer.new()
 	add_child(countdown_timer)
-	countdown_timer.connect("timeout", _on_timer_timeout)
+	countdown_timer.connect("timeout", on_timer_timeout)
 	countdown_timer.start()
 
-func _on_timer_timeout():
+func on_timer_timeout():
 	countdown -= 1
 	var remaining_time = $Panel1.get_node("time_number_label").text.to_int() - 1
 	$Panel1.get_node("time_number_label").set_text(str(remaining_time))
 	if $Panel1.get_node("time_number_label").text.to_int() <= 0:
 		$Panel3.show()
 		enabel_disable_basket_button(true)
-		await get_tree().create_timer(1).timeout 
+		await get_tree().create_timer(1).timeout
 		$Panel3.hide()
 		enabel_disable_basket_button(false)
 		set_level_game()
@@ -58,12 +60,26 @@ func set_level_game():
 	else:
 		point = $Panel.get_node("point_number_label").text.to_int() + 1
 	$Panel.get_node("point_number_label").set_text(str(point))
-	_choose_ball_and_basket_random_image()
+	choose_ball_and_basket_random_image()
 	$Panel1.get_node("time_number_label").set_text(str(3))
 	countdown_timer.stop()
-	_create_time()
+	create_time()
 
 func enabel_disable_basket_button(state):
 	for i in range(instance_basket_scene.get_child_count()):
 		var texture_button = instance_basket_scene.get_child(i)
 		texture_button.set_disabled(state)
+
+func exit():
+	get_tree().set_pause(!get_tree().is_paused())
+	$Popup.show()
+	$Popup.get_node("ok_button").pressed.connect(go_to_main)
+	$Popup.get_node("cancel_button").pressed.connect(cancel_exit)
+
+func go_to_main():
+	get_tree().set_pause(!get_tree().is_paused())
+	get_tree().change_scene_to_file('res://Scene/main.tscn')
+
+func cancel_exit():
+	get_tree().set_pause(!get_tree().is_paused())
+	$Popup.hide()
