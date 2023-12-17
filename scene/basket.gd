1101f3214bd7c82857508e4e743ed3069d640289
unknown <saririroya@gmail.com>
2023-12-17 23:11:03 +0330

    change image basket and ball to color

--- scene/basket.gd
+++ scene/basket.gd
@@ -1,28 +1,17 @@
 extends Node2D
 
-var images = [] # لیست عکس‌ها را در اینجا ذخیره کنید
+@onready var global = get_node("/root/Global")
 
 func _ready():
 	choose_random_image_for_each_basket()
 
 func choose_random_image_for_each_basket():
-		# اضافه کردن تمام عکس‌ها به لیست
-	images.append(preload("res://sprite/basket/pink_basket.png"))
-	images.append(preload("res://sprite/basket/yellow_basket.png"))
-	images.append(preload("res://sprite/basket/orange_basket.png"))
-	images.append(preload("res://sprite/basket/purple_basket.png"))
-	images.append(preload("res://sprite/basket/blue_basket.png"))
-	images.append(preload("res://sprite/basket/green_basket.png"))
-	# تنظیم تصادفی عکس برای هر اسپرایت
-	for i in range(get_child_count()):
-		var texture_button = get_child(i)
-		randomize_image(texture_button)
+	global.remaining_color_lst = global.color_lst.duplicate(true)
+	for child in get_children():
+		if child is TextureButton:
+			randomize_image(child)
 
 func randomize_image(texture_button):
-	if images.size() == 0:
-		print("لیست عکس‌ها خالی است!")
-		return
-
-	var index = randi() % images.size()
-	texture_button.set_texture_normal(images[index])
-	images.remove_at(index)	
+	var index = randi() % global.remaining_color_lst.size()
+	texture_button.modulate = global.remaining_color_lst[index]
+	global.remaining_color_lst.remove_at(index)
--- scene/game.gd
+++ scene/game.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-var countdown : int = 3  # عدد ابتدایی
+var countdown : int = 3
 var countdown_timer : Timer
 
 var main_scene = load("res://scene/main.tscn")
-var ball_scene = load("res://scene/ball.tscn")
 var basket_scene = load("res://scene/basket.tscn")
+var ball_scene = load("res://scene/ball.tscn")
 
 var instance_main_scene = main_scene.instantiate()
-var instance_ball_scene = ball_scene.instantiate()
 var instance_basket_scene = basket_scene.instantiate()
+var instance_ball_scene = ball_scene.instantiate()
 
 @onready var global = get_node("/root/Global")
 
@@ -21,17 +21,15 @@ func _ready():
 	$Popup.hide()
 	$exit_texture_button.pressed.connect(exit)
 	create_time()
-	add_child(instance_ball_scene)
 	add_child(instance_basket_scene)
+	add_child(instance_ball_scene)
 
 	for i in range(instance_basket_scene.get_child_count()):
 		var texture_button = instance_basket_scene.get_child(i)
 		texture_button.pressed.connect(_on_button_pressed.bind(texture_button, instance_ball_scene.get_child(0)))
 
 func _on_button_pressed(basket, ball):
-	var basket_color = basket.texture_normal.resource_path.get_file().split("_")[0]
-	var ball_color = ball.texture.resource_path.get_file().split("_")[0]
-	if basket_color == ball_color:
+	if basket.modulate == ball.modulate:
 		$Panel3.get_node("message_label").set_text("Correct")
 		$Panel3.show()
 		var point = $Panel.get_node("point_number_label").text.to_int() + 1
@@ -47,8 +45,8 @@ func _on_button_pressed(basket, ball):
 
 func choose_ball_and_basket_random_image():
 	var script2d_ball = instance_ball_scene.get_node("Sprite2D")
-	script2d_ball.randomize_image()
 	instance_basket_scene.choose_random_image_for_each_basket()
+	script2d_ball.randomize_image()
 
 func create_time():
 	countdown_timer = Timer.new()
